d717acb531ff8146daa3162e812b2174d7cd9b49
MatteoBucc <mbuccolieri22@gmail.com>
2025-10-29 14:50:33 +0100

    fix hydration clienr

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // 👈 importa questo
 
 import { routes } from './app-routing.module';
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient() // 👈 aggiungi questo
   ]
 };
